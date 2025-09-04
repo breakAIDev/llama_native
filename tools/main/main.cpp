5feec27cdbbc3da95cb7794aa308f8978fa6f6bc
breakAIDev <tonywilson930727@gmail.com>
2025-09-04 16:46:13 +0700

    no_think mode

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -1252,7 +1252,7 @@ int main(int argc, char ** argv) {
 #ifdef HAVE_VOICE_IO
                 // block until we get a final utterance from Vosk
                 buffer = g_voice.wait_utt();
-                LOG_INF("[mic] %s\n", buffer.c_str());
+                LOG_INF("%s/no_think\n", buffer.c_str());
 #else
                 if (!params.input_prefix.empty() && !params.conversation_mode) {
                     LOG_DBG("appending input prefix: '%s'\n", params.input_prefix.c_str());
